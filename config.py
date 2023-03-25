30a0d87c6f5f695ae7f0f7ec6afca31b1dec3df9
Yusuf <93678702+ArapSikenGenc@users.noreply.github.com>
2023-03-25 19:35:14 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("5926297757:AAHXHx-FMkpg16_ShWykWadi1ZfaZ3Hr7ys")
+API_ID = int(getenv("API_ID", "2013655"))
+API_HASH = getenv("036e4559f48a51ef4e33b2d6a1e7f502")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "90"))
-ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+ASSISTANT_PREFIX = list(getenv("1ApWapzMBu4gzcWt7wBJRxLC5ESqVD3GkPpABDhuPETwm0Wsv_l-2CkjjYXXV6LALFS4uF4NINjWsF2dWJOPHs_BhIwbsaSBo_z4gOsCbwIt_8FumLROxKvj3d-2fP0t_24N7X3tadSdLiqqufzetU4TI0OszsCPnZz4IYeVNXwSmH7y1CEEXdNVtxyi2YTgSqwsss9QI2qPNXnRdPK5PwQSFGf0HZY2aX-VvW8EyQBjuxjfdKjWkR8Bv0z4zv3IjLT7OmYaGkRuv8mTJeRl4M5ZSUUea2o7v5j_EdRNx9uNueYdGE-fH8p0GettZsCQRuJTDRja4a98EG7IAz8R1XooY0mi_wG0=", ".").split())
+MONGO_DB_URI = getenv("mongodb+srv://Patuska:Ismet@patuska.vjubctv.mongodb.net/?retryWrites=true&w=majority")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1300527267").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5026921065").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001811271788"))
+MUSIC_BOT_NAME = getenv("Patuska")
+#HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+#HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", "https://github.com/ArapSikenGenc/maho"
